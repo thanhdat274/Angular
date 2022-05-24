6aff38cf8fac7dc2a16c05d2b3ae43aab04a703c
thanhdat274 <nguyenthanhdat27042002@gmail.com>
2022-05-24 21:50:05 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ChampComponent } from './champ/champ.component';
@@ -34,6 +34,7 @@ import { UserFormComponent } from './user/form/form.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     NgbModule,
     FormsModule
   ],
